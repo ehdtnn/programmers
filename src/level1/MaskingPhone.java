c7cb000c4d8483fc326c4a15c623d3895ef6bc12
ehdtnn <ehdtnn@gmail.com>
2021-11-12 00:05:38 +0900

    solve

--- src/level1/MaskingPhone.java
+++ src/level1/MaskingPhone.java
@@ -2,19 +2,22 @@ package level1;
 
 public class MaskingPhone {
     public String solution(String phone_number) {
+        char[] ch = phone_number.toCharArray();
+        for (int i = 0; i < ch.length - 4; i ++) {
+            ch[i] = '*';
+        }
+        return String.valueOf(ch);
+    }
+    /*
+    public String solution(String phone_number) {
         String answer = "";
+        String regex = phone_number.substring(0, phone_number.length()-4);
+        String replace = "";
+        for (int i = 0; i < regex.length(); i++) {
+            replace += "*";
+        }
+        answer = phone_number.replaceAll(regex, replace);
         return answer;
     }
-
-    public static void main(String[] args) {
-
-        String str = "01092460054";
-        String masking = str.substring(0, str.length()-4);
-        String a = "";
-        for (int i = 0; i < masking.length(); i++) {
-            a+= "*";
-        }
-        String b = str.replaceAll(masking, "*");
-        System.out.println(a);
-    }
+    */
 }
