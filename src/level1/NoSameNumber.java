c5bbc5e4883c3961f43fb42349f3fc16da8bf5cb
ehdtnn <ehdtnn@gmail.com>
2021-11-12 23:59:43 +0900

    refactor

--- src/level1/NoSameNumber.java
+++ src/level1/NoSameNumber.java
@@ -4,24 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NoSameNumber {
-    public static void main(String[] args) {
-
-        int[] arr = {1,1,3,3,0,1,1};
+    public int[] solution(int []arr) {
         int[] answer = {};
 
         List<Integer> list = new ArrayList<>();
-        // 연속적인 숫자 제거
-        if (arr.length > 0) {
-            list.add(arr[0]);
-        }
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] == arr[i-1]) {
+        int pre = 10;
+        for (int num : arr) {
+            if (num == pre) {
                 continue;
-            } else {
-                list.add(arr[i]);
             }
+            list.add(num);
+            pre = num;
         }
         answer = list.stream().mapToInt(i->i).toArray();
 
+        return answer;
     }
 }
