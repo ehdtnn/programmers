2df58bda82d99cd5c45b236897f36b9e7d1ad876
ehdtnn <ehdtnn@gmail.com>
2021-11-17 00:14:41 +0900

    solve

--- src/level1/Collatz.java
+++ src/level1/Collatz.java
@@ -1,29 +1,18 @@
 package level1;
 
 public class Collatz {
-    public int solution(int num) {
+    int solution(int num) {
         int answer = 0;
+        long number = num;
 
-        if (num == 1) {
-            return num;
+        while (number != 1) {
+            if (answer == 500) {
+                return -1;
+            }
+            answer++;
+            number = number % 2 == 0 ? number / 2 : number * 3 + 1;
         }
 
-        int i = 1;
-        while (num != 1) {
-            if (i == 500) {
-                num = -1;
-                break;
-            }
-            if (num % 2 == 0) {
-                num = num / 2;
-            } else {
-                num = (num * 3) + 1;
-            }
-            i++;
-            System.out.println("num : " + num);
-        }
-        answer = num;
-        System.out.println("answer : " + answer);
         return answer;
     }
 }
