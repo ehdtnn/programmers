7d082125637cc53746683d5a766e4bf7ef8a7a1d
ehdtnn <ehdtnn@gmail.com>
2021-11-19 00:12:42 +0900

    solving

--- src/level1/Sum.java
+++ src/level1/Sum.java
@@ -3,7 +3,20 @@ package level1;
 public class Sum {
     public long solution(int a, int b) {
         long answer = 0;
+        int bigger = 0;
+        int smaller = 0;
 
+        if (a - b > 0) {
+            bigger = a;
+            smaller = b;
+        } else if (a - b < 0) {
+            bigger = b;
+            smaller = a;
+        } else {
+            return a;
+        }
+
+        answer = (  bigger * (bigger + 1) / 2) - smaller;
         return answer;
     }
 }
