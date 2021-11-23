4eef11fb0c01f36d19485892b74c99c2e54c6c1a
ehdtnn <ehdtnn@gmail.com>
2021-11-23 23:59:16 +0900

    solve

--- src/level1/SumOfX.java
+++ src/level1/SumOfX.java
@@ -4,8 +4,9 @@ public class SumOfX {
     public long[] solution(int x, int n) {
         long[] answer = new long[n];
 
+        long xx = x;
         for (int i = 0; i < n; i++) {
-            answer[i] = x * (i + 1);
+            answer[i] = xx * (i + 1);
         }
 
         return answer;
