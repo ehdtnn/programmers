d8187f8b66c862013c9e05464f77a9abfe7a4041
ehdtnn <ehdtnn@gmail.com>
2021-12-11 23:40:48 +0900

    solving

---
+++ src/level2/MenuRenewal.java
@@ -0,0 +1,8 @@
+package level2;
+
+public class MenuRenewal {
+    public String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
+        return answer;
+    }
+}
---
+++ src/level2/NewsClustering.java
@@ -0,0 +1,8 @@
+package level2;
+
+public class NewsClustering {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        return answer;
+    }
+}
