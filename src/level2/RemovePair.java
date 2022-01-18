ee92fbfeac52f20a4b991f891f15cb716fea0de0
ehdtnn <ehdtnn@gmail.com>
2022-01-18 23:57:25 +0900

    solving

--- src/level2/RemovePair.java
+++ src/level2/RemovePair.java
@@ -1,24 +1,26 @@
 package level2;
 
 public class RemovePair {
-    public int solution(String s) {
-        int answer = -1;
-        for (int i = 0; i < s.length(); i++) {
-            if (i == s.length() - 1) {
-                return -1;
-            }
-            if (s.charAt(i) == s.charAt(i+1)) {
-                s = s.substring(2, s.length());
-            }
-        }
 
-        //test();
+    public static void main(String[] args) {
+        RemovePair.solution("baabaa");
+    }
+
+    public static int solution(String s) {
+        int answer = -1;
+
+
+        System.out.println(s.substring(0, 1) + s.substring(3,6));
+
 
         return answer;
     }
 
-    public static void test() {
-        String str = "abc";
-        System.out.println(str.substring(2, str.length()));
+    public static String extract(String str, int index) {
+        String head = str.substring(1, index);
+        System.out.println(head);
+        String tail = str.substring(index+1, index+1+1);
+        System.out.println(tail);
+        return head + tail;
     }
 }
---
+++ src/level2/RotationOfMatrix.java
@@ -0,0 +1,8 @@
+package level2;
+
+public class RotationOfMatrix {
+    public int[] solution(int rows, int columns, int[][] queries) {
+        int[] answer = {};
+        return answer;
+    }
+}
